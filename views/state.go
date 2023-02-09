2f8522ba8af2188930219dcd32a60924a9557e09
nandotmbn <orlandopratama695@gmail.com>
2023-02-10 01:52:15 +0700

    fix: state datatype

--- views/state.go
+++ views/state.go
@@ -3,19 +3,19 @@ package views
 import "time"
 
 type PayloadState struct {
-	State   bool    `json:"state" validate:"required"`
-	Battery float32 `json:"battery" validate:"required"`
+	State   bool    `json:"state"`
+	Battery float32 `json:"battery"`
 }
 
 type LastState struct {
-	State     bool      `json:"state" validate:"required"`
-	Battery   float32   `json:"battery" validate:"required"`
+	State     bool      `json:"state"`
+	Battery   float32   `json:"battery"`
 	UpdatedAt time.Time `json:"updated_at" bson:"updated_at"`
 	CreatedAt time.Time `json:"created_at" bson:"created_at"`
 }
 
 type FinalState struct {
-	State     bool      `json:"state" validate:"required"`
-	Battery   float32   `json:"battery" validate:"required"`
+	State     bool      `json:"state"`
+	Battery   float32   `json:"battery"`
 	UpdatedAt time.Time `json:"updated_at" bson:"created_at"`
 }
